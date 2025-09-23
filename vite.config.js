f7e3b5ac18289475968c3c1081802d565fd5554a
Atharv Kundalkar <atharvkundalkar1@gmail.com>
2025-09-23 15:36:59 +0000

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   server: {
-    host: '0.0.0.0',
+    host:'0.0.0.0',
+
+    allowedHosts: [
+      '21a46cc1-aebf-4b70-8c23-c0729486d77c-00-1tlug3eqiud.pike.replit.dev'
+    ]
+   
   }
 })
